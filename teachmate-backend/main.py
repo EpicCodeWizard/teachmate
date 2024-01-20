ae55a204b7e47f3cbef160c6c0229c0f72a766a7
sarve <epiccodewizard@gmail.com>
2024-01-20 14:04:24 -0800

    Lol

--- teachmate-backend/main.py
+++ teachmate-backend/main.py
@@ -1,3 +1,9 @@
 from quart import Quart
 
-app = Quart()
+app = Quart(__name__)
+
+@app.route("/")
+async def main():
+    return "hi"
+
+app.run(host="localhost", port=8080)
--- teachmate/src/app/page.tsx
+++ teachmate/src/app/page.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
+import { Flex, Text, Button } from '@radix-ui/themes';
 
-export default function Home() {
+export default function MyApp() {
   return (
-    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
-      </div>
-    </div>
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let{"'"}s go</Button>
+    </Flex>
   );
 }
