eb3f2c26e0dadca302b9ecdbb0c2ae6d595d77bf
mc08 <mc08>
2024-01-20 14:52:25 -0800

    w

--- teachmate/src/app/layout.tsx
+++ teachmate/src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,85 +20,83 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Theme>
-            <div className="navbar bg-base-100">
-              <div className="navbar-start">
-                <div className="dropdown">
-                  <div
-                    tabIndex={0}
-                    role="button"
-                    className="btn btn-ghost lg:hidden"
+          <div className="navbar bg-base-100">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h8m-8 6h16"
-                      />
-                    </svg>
-                  </div>
-                  <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-                  >
-                    <li>
-                      <a>Item 1</a>
-                    </li>
-                    <li>
-                      <a>Parent</a>
-                      <ul className="p-2">
-                        <li>
-                          <a>Submenu 1</a>
-                        </li>
-                        <li>
-                          <a>Submenu 2</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a>Item 3</a>
-                    </li>
-                  </ul>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-              </div>
-              <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
                   <li>
                     <a>Item 1</a>
                   </li>
                   <li>
-                    <details>
-                      <summary>Parent</summary>
-                      <ul className="p-2">
-                        <li>
-                          <a>Submenu 1</a>
-                        </li>
-                        <li>
-                          <a>Submenu 2</a>
-                        </li>
-                      </ul>
-                    </details>
+                    <a>Parent</a>
+                    <ul className="p-2">
+                      <li>
+                        <a>Submenu 1</a>
+                      </li>
+                      <li>
+                        <a>Submenu 2</a>
+                      </li>
+                    </ul>
                   </li>
                   <li>
                     <a>Item 3</a>
                   </li>
                 </ul>
               </div>
-              <div className="navbar-end">
-                <a className="btn">Button</a>
-              </div>
+              <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <details>
+                    <summary>Parent</summary>
+                    <ul className="p-2">
+                      <li>
+                        <a>Submenu 1</a>
+                      </li>
+                      <li>
+                        <a>Submenu 2</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+            <div className="navbar-end">
+              <a className="btn">Button</a>
+            </div>
+          </div>
 
-            {children}
-          </Theme>
+          {children}
         </body>
       </html>
     </ClerkProvider>
