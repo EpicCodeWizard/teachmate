6e765f7a7a7d9f50072be5870a640828c7b53938
sarve <epiccodewizard@gmail.com>
2024-01-20 17:00:53 -0800

    Lol2

--- teachmate-backend/main.py
+++ teachmate-backend/main.py
@@ -1,9 +1,23 @@
-# from quart import Quart
-#
-# app = Quart(__name__)
-#
-# @app.route("/")
-# async def main():
-#     return "hi"
-#
-# app.run(host="localhost", port=8080)
+from quart import Quart
+import requests
+
+url = "https://api.vectara.io/v1/upload"
+
+payload={}
+headers = {
+    'Content-Type': 'multipart/form-data',
+    'Accept': 'application/json',
+    'x-api-key': '<API_KEY_VALUE>'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+app = Quart(__name__)
+
+@app.route("/")
+async def main():
+    return "hi"
+
+app.run(host="localhost", port=8080)
