c31d792737d7584416a934f9dad3186b1c9c9eb4
cryptopig <vpig76@gmail.com>
2024-01-20 19:49:11 -0800

    s

--- teachmate/src/app/layout.tsx
+++ teachmate/src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script'
-import { SessionProvider } from "next-auth/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
