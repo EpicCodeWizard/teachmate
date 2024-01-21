31512e51ad12c1e7f5fec27a3a40baa610a56a1e
cryptopig <71409503+cryptopig@users.noreply.github.com>
2024-01-20 23:34:16 -0800

    s

--- teachmate/src/app/layout.tsx
+++ teachmate/src/app/layout.tsx
@@ -1,8 +1,16 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from 'next/script'
+import Script from "next/script";
+
+<></>;
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,40 +24,48 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     // <SessionProvider session={session}>
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <div className="navbar bg-base-100">
-            <div className="navbar-start">
-              <a className="btn btn-ghost text-xl" href="/">TeachMate</a>
+      <>
+        <html lang="en">
+          <body className={inter.className}>
+            <div className="navbar bg-base-100">
+              <div className="navbar-start">
+                <a className="btn btn-ghost text-xl" href="/">
+                  TeachMate
+                </a>
+              </div>
+              <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                  <li>
+                    <a href="/about">About</a>
+                  </li>
+                  <li>
+                    <a href="/attendance">Attendance</a>
+                  </li>
+                  <li>
+                    <a href="/feedback">Feedback</a>
+                  </li>
+                  <li>
+                    <a href="/slideshow">Slideshow</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="navbar-end">
+                <SignedIn>
+                  <UserButton afterSignOutUrl="/"></UserButton>
+                </SignedIn>
+                <SignedOut>
+                  <SignInButton mode="modal"></SignInButton>
+                </SignedOut>
+              </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-              <ul className="menu menu-horizontal px-1">
-                <li>
-                  <a href="/about">About</a>
-                </li>
-                <li>
-                  <a href="/attendance">Attendance</a>
-                </li>
-                <li>
-                  <a href="/feedback">Feedback</a>
-                </li>
-                <li>
-                  <a href="/slideshow">Slideshow</a>
-                </li>
-            </div>
-            <div className="navbar-end">
-              <SignedIn><UserButton afterSignOutUrl="/"></UserButton></SignedIn>
-              <SignedOut><SignInButton mode="modal"></SignInButton></SignedOut>
-            </div>
-          </div>
-          {children}
-          <Script src="/script.js" />
-        </body>
-      </html>
+            {children}
+            <Script src="/script.js" />
+          </body>
+        </html>
+      </>
     </ClerkProvider>
     // </SessionProvider>
   );
